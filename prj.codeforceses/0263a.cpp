b209dd27e5de5101c29aaeec3e942a2667b2c10c
Михаил Емельянцев <94082288+MiamiofMB@users.noreply.github.com>
2024-02-27 13:40:41 +0300

    Update 0263a.cpp

--- prj.codeforceses/0263a.cpp
+++ prj.codeforceses/0263a.cpp
@@ -1,1 +1,28 @@
 
+
+#include <iostream> 
+#include <vector> 
+#include <algorithm> 
+#include <unordered_map> 
+
+int main() {
+	std::pair<int, int> save;
+	for (int i = 1;i < 6;i++) {
+		for (int j = 1;j < 6;j++) {
+			int x;
+			std::cin >>x;
+			if (x == 1) {
+				save.first = i;
+				save.second = j;
+				break;
+			}
+			
+		}
+	}
+	
+	int f = std::abs(3 - save.first) + std::abs(3 - save.second);
+	std::cout << f;
+
+
+
+}
