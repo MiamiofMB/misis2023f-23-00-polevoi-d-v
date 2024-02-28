2f1c4ccced2c5e703dd979affd808e2c4e956955
Михаил Емельянцев <94082288+MiamiofMB@users.noreply.github.com>
2024-02-28 14:46:34 +0300

    Update 276a.cpp

--- prj.codeforceses/276a.cpp
+++ prj.codeforceses/276a.cpp
@@ -1,1 +1,26 @@
 
+
+
+#include <iostream> 
+#include <vector> 
+#include <algorithm> 
+#include <unordered_map> 
+#include <set>
+
+int main() {
+	int n, k,f,t;
+	std::cin >> n >> k;
+	std::vector<int> res;
+	for (int i = 0;i < n;i++) {
+		std::cin >> f >> t;
+		if (t > k) {
+			res.push_back(f - (t - k));
+
+		}
+		else {
+			res.push_back(f);
+		}
+	}
+	auto r = *std::max_element(res.begin(),res.end());
+	std::cout << r;
+}
