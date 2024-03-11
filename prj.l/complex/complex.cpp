da0dfa16a62b4420ee6a1fb789f9750ea32b23b3
Михаил Емельянцев <94082288+MiamiofMB@users.noreply.github.com>
2024-03-11 14:04:51 +0300

    Create complex.cpp

---
+++ prj.l/complex/complex.cpp
@@ -0,0 +1,50 @@
+#include <complex/complex.hpp>
+
+Complex& Complex::operator+=(const double rhs) noexcept{
+  re+=rhs;  
+  return *this;
+}
+Complex& Compex::operator+=(const Complex& rhs) noexcept{
+  re+=rhs.re;
+  im+=rhs.im;
+  return *this;
+}
+
+Complex& Complex::operator-=(const double rhs) noexcept{
+  re-=rhs;  
+  return *this;
+}
+Complex& Compex::operator-=(const Complex& rhs) noexcept{
+  re-=rhs.re;
+  im-=rhs.im;
+  return *this;
+}
+
+Complex& Complex::operator*=(const double rhs) noexcept{
+  re*=rhs;  
+  return *this;
+}
+Complex& Compex::operator*=(const Complex& rhs) noexcept{
+  re*=rhs.re;
+  im&=rhs.im;
+  return *this;
+}
+
+Complex& Complex::operator/=(const double rhs){
+  re/=rhs;  
+  return *this;
+}
+Complex& Compex::operator/=(const Complex& rhs){
+  re/=rhs.re;
+  im/=rhs.im;
+  return *this;
+}
+
+
+
+ bool Complex::operator==(Complex& rhs) const noexcept {if (re==rhs.re && im==rhs.im){return true;}else{return false}}
+ bool Complex::operator!=(Complex& rhs) const noexcept {if (re==rhs.re && im==rhs.im){return false;}else{return true}}
+
+
+
+
