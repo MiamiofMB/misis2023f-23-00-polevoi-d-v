2058074f33a94685709633a640c2d53edfef6b03
Михаил Емельянцев <94082288+MiamiofMB@users.noreply.github.com>
2024-03-11 14:49:53 +0300

    Update complex.cpp

--- prj.l/complex/complex.cpp
+++ prj.l/complex/complex.cpp
@@ -1,5 +1,10 @@
 #include <complex/complex.hpp>
 
+
+//Унарный минус
+Complex Complex::operator-() noexcept{return Complex(-re,-im);}
+
+
 Complex& Complex::operator+=(const double rhs) noexcept{
   re+=rhs;  
   return *this;
@@ -46,7 +51,54 @@ Complex& Compex::operator/=(const Complex& rhs){
  bool Complex::operator!=(Complex& rhs) const noexcept {if (re==rhs.re && im==rhs.im){return false;}else{return true}}
 
 
-Complex Complex::operator+()
+Complex Complex::operator+(Complex& lhs, Complex& rhs) noexcept{
+  return Complex(rhs.re+lhs.re,rhs.im+lhs.im);
+}
+Complex Complex::operator+(double lhs, Complex& rhs) noexcept{
+  return Complex(rhs+lhs.re,lhs.im);
+}
+Complex Complex::operator+(Complex& lhs, double rhs) noexcept{ 
+  return Complex(rhs.re+lhs,rhs.im);
+}
+
+
+
+Complex Complex::operator-(Complex& lhs, Complex& rhs) noexcept{
+  return Complex(lhs.re-rhs.re,lhs.im-rhs.im);
+}
+Complex Complex::operator-(double lhs, Complex& rhs) noexcept{
+  return Complex(lhs-rhs.re,rhs.im);
+}
+Complex Complex::operator-(Complex& lhs, double rhs) noexcept{ 
+  return Complex(lhs.re-rhs,lhs.im);
+}
+
+
+Complex Complex::operator*(Complex& lhs, Complex& rhs) noexcept{
+  return Complex(lhs.re*rhs.re,lhs.im*rhs.im);
+}
+Complex Complex::operator*(double lhs, Complex& rhs) noexcept{
+  return Complex(lhs*rhs.re,rhs.im);
+}
+Complex Complex::operator*(Complex& lhs, double rhs) noexcept{ 
+  return Complex(lhs.re*rhs,lhs.im);
+}
+
+
+Complex Complex::operator/(Complex& lhs, Complex& rhs){
+  return Complex(lhs.re/rhs.re,lhs.im/rhs.im);
+}
+Complex Complex::operator/(double lhs, Complex& rhs) noexcept{
+  return Complex(lhs/rhs.re,rhs.im);
+}
+Complex Complex::operator/(Complex& lhs, double rhs) noexcept{ 
+  return Complex(lhs.re/rhs,lhs.im);
+}
+
+
+std::ostream& Complex::WriteTo(std::ostream& ostrm) const noexcept { return ostrm; }
+std::istream& Complex::ReadFrom(std::istream& istrm) noexcept { return istrm; }
+
 
 
 
