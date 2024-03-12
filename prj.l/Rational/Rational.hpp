f204ac809fdc3249b36ab964344f3d4298a53c7c
Михаил Емельянцев <94082288+MiamiofMB@users.noreply.github.com>
2024-03-12 15:53:50 +0300

    Update Rational.hpp

--- prj.l/Rational/Rational.hpp
+++ prj.l/Rational/Rational.hpp
@@ -12,18 +12,18 @@ public:
 	//ячейки памяти 
 	std::int64_t top{ 0 };//числитель
 	std::int64_t bot{ 1 };//знаменатель
-	
+
 	//конструкторы и прочая хрень
 	Rational() = default;
-	explicit Rational(std::int64_t t, std::int64_t b) { top = t;bot = b; if (bot == 0){throw std::invalid_argument("Zero denumenator in Rational ctor")}}
-	Rational(std::int64_t x) { top = x;bot = 1;}
+	explicit Rational(std::int64_t t, std::int64_t b) { top = t;bot = b; if (bot == 0) { throw std::invalid_argument("Zero denumenator in Rational ctor") } }
+	Rational(std::int64_t x) { top = x;bot = 1; }
 	Rational(Rational&) = default;
 	Rational(Rational&&) = default;
 	~Rational() = default;
 
 	//геттеры
-	[[nodiscart]] double top() noexcept { return top; }
-	[[nodiscart]] double bottom() noexcept { return bot; }
+	[[nodiscart]] std::int64_t top() noexcept { return top; }
+	[[nodiscart]] std::int64_t bottom() noexcept { return bot; }
 
 	//операторы изменения
 	Rational& operator +=(const Rational& rhs) noexcept;
@@ -34,9 +34,15 @@ public:
 
 	Rational& operator /=(const Rational& rhs);
 	Rational& operator /=(std::int64_t rhs);
-	
+
 	Rational& operator *=(const Rational& rhs) noexcept;
 	Rational& operator *=(std::int64_t rhs) noexcept;
+
+	Rational& operator++() noexcept;
+	Rational& operator--() noexcept;
+
+
+	[[nodiscard]] Rational operator-() const noexcept { return { -top, -bot }; } // унарный минус
 
 	//операторы сравнения
 	[[nodiscard]] bool operator<=(Rational& rhs) noexcept;
@@ -45,6 +51,8 @@ public:
 	[[nodiscard]] bool operator>(Rational& rhs) noexcept;
 	[[nodiscard]] bool operator==(Rational& rhs) noexcept;
 	[[nodiscard]] bool operator!=(Rational& rhs) noexcept;
+	[[nodiscard]] bool operator==(int64_t rhs) noexcept;
+	[[nodiscard]] bool operator!=(int64_t rhs) noexcept;
 
 
 
@@ -52,7 +60,7 @@ public:
 
 //операторы создания
 
-[[nodiscard]] Rational operator+(Rational& lhs,Rational& rhs) noexcept;
+[[nodiscard]] Rational operator+(Rational& lhs, Rational& rhs) noexcept;
 [[nodiscard]] Rational operator+(const  std::int64_t lhs, Rational& rhs) noexcept;
 [[nodiscard]] Rational operator+(Rational& lhs, const std::int64_t rhs) noexcept;
 
@@ -75,4 +83,13 @@ std::ostream& operator<<(std::ostream& ostrm, const Rational& rhs) noexcept;
 std::istream& operator>>(std::istream& istrm, Rational& rhs) noexcept;
 
 #endif 
+
+
+
+
+
+
+
+
+
 
