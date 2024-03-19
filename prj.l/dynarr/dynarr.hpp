9135561427f8481d960876a6ccc684f1b07d69fa
Михаил Емельянцев <94082288+MiamiofMB@users.noreply.github.com>
2024-03-20 00:32:21 +0300

    Create dynarr.hpp

---
+++ prj.l/dynarr/dynarr.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef DYNARR_DYNARR_HPP_20231203
+#define DYNARR_DYNARR_HPP_20231203
+#include <cstddef>
+
+
+
+class dynarr {
+private:
+	int64_t size = 0;
+	float* data = new float[0];
+public:
+	dynarr() = default;
+	dynarr(const int64_t siz) { delete data;data = new float[siz];size = siz; }
+	dynarr(dynarr&) = default;
+	dynarr(dynarr&&) = default;
+	~dynarr() { delete data; data = nullptr; }
+	float operator[](int indx) const;
+	void push_back(float ins) const;
+	void resize(int new_size);
+	[[nodiscard]] int size() const noexcept;
+
+};
+
+
+
+
+
+#endif
