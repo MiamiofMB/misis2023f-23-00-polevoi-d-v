714bc4e442d223b15f0be30fe856f04a0f44ffc4
Михаил Емельянцев <94082288+MiamiofMB@users.noreply.github.com>
2024-03-20 00:57:39 +0300

    Update dynarr.cpp

--- prj.l/dynarr/dynarr.cpp
+++ prj.l/dynarr/dynarr.cpp
@@ -1,7 +1,24 @@
 #include <cstddef>
 #include<iostream>
+#include<stdexcept>
 #include<dynarr/dynarr.hpp>
 
 dynarr::dynarr(const dynarr& nw) { std::swap(size, nw.size);std::swap(data, nw.data);std::swap(capacity, nw.capacity);}
 
-dynarr::
+float dynarr::operator[] (int indx) {
+  if(indx >0 and indx<capacity-1){
+    return data[indx];
+  }
+}
+void dynarr::push_back(float ins){
+    size+=1;
+    capacity+=1;
+    float* temp = new float[size];
+    for (int i=0; i<size-1;i++){
+      temp[i] = size[i];
+    } 
+    temp[size-1] = ins;
+    delete data;
+    data = temp;
+    temp = nullptr;
+}
