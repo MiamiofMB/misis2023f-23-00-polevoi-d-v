9516b73a18b772a2a5d15958745efcbac854d9b4
Михаил Емельянцев <94082288+MiamiofMB@users.noreply.github.com>
2024-03-25 10:18:38 +0300

    Update dynarr.cpp

--- prj.l/dynarr/dynarr.cpp
+++ prj.l/dynarr/dynarr.cpp
@@ -3,7 +3,28 @@
 #include<stdexcept>
 #include<dynarr/dynarr.hpp>
 
-dynarr::dynarr(const dynarr& nw) { std::swap(size, nw.size);std::swap(data, nw.data);std::swap(capacity, nw.capacity);}
+dynarr::dynarr(dynarr&& nw) noexcept { std::swap(size, nw.size);std::swap(data, nw.data);std::swap(capacity, nw.capacity);return *this;}
+
+dyanrr::dynarr(dynarr& nw){
+  if (nw.size<=size){
+    capacity = nw.size;
+  }
+  else{
+    capacity = size;
+  }
+  float* temp = new float[nw.size]{0};
+  std::copy(data,data+nw.size,temp);
+  size = nw.size;
+  delete data;
+  data = temp;
+  temp = null_ptr;
+   
+  return *this;
+
+  
+}
+
+
 
 float dynarr::operator[] (int indx) {
   if(indx >0 and indx<size){
@@ -12,27 +33,18 @@ float dynarr::operator[] (int indx) {
   else{throw std::invalid_argument("array index out of range");}
 }
 void dynarr::push_back(float ins){
-    size+=1;
-    capacity+=1;
-    float* temp = new float[size];
-    for (int i=0; i<size;i++){
-      temp[i] = size[i];
-    } 
-    temp[size-1] = ins;
-    delete data;
-    data = temp;
-    temp = nullptr;
+  dynarr::resize(size+1);
+  data[size-1] = ins;
 }
 void dynarr::resize(int new_size) const{
   if (new_size<=0){
     throw std::invalid_argument("Size can't be a negative value,you donkey!");
   }
   
-  if (new_size<size){
+  if (new_size<=size){
     size= new_size;
   }
   else{
-    capacity = 0;
     float* temp = new float[new_size]{0};
     std::copy(data,data+size,temp);
     delete data;
@@ -42,4 +54,4 @@ void dynarr::resize(int new_size) const{
     size = new_size;
     }
 }
-int dynarr:size(){return size;}
+int dynarr::size(){return size;}
