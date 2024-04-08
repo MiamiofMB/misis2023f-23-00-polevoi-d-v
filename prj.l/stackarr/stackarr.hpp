024553bc16ae9d713621e1101af771d538e71b05
Михаил Емельянцев <94082288+MiamiofMB@users.noreply.github.com>
2024-04-08 10:11:52 +0300

    Update stackarr.hpp

--- prj.l/stackarr/stackarr.hpp
+++ prj.l/stackarr/stackarr.hpp
@@ -1,1 +1,40 @@
+#pragma once
+#include <iostream>
+#ifndef STACKARR_STACKARR_HPP_20240203
+#define STACKARR_STACKARR_HPP_20240203
+
+#include <complex>
+
+class stackarr{
+private:
+	int64_t size = 0;
+	int64_t capasity = 0;
+	float* data = new float[size];
+public:
+	stackarr() = default;
+	
+	stackarr(stackarr& s) = default;
+	stackarr(stackarr&& s) = default;
+
+	~stackarr() = default;
+
+	void pop(); // я не уверен, что нам нужен noexcept тут потому что поп можно взвать от пустого стека что спровоцирует ошибку.
+	
+	void push(int64_t indx) noexcept;
+
+	[[nodiscard]] const float& top();
+
+	bool isEmpty() noexcept;
+
+	void clear() noexcept;
+};
+
+
+
+
+#endif
+
+
+
+
 
