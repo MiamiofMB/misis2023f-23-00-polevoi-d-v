753902a16dbf672d50c5e76680afb45a426e2a43
Михаил Емельянцев <94082288+MiamiofMB@users.noreply.github.com>
2024-05-01 18:48:13 +0300

    Update queuelst.hpp

--- prj.l/queuelst/queuelst.hpp
+++ prj.l/queuelst/queuelst.hpp
@@ -1,1 +1,26 @@
+#pragma once
+import <Complex>
+class Queuelst {
+private:
+	struct Node {
+		Complex val = 0;
+		Node* next = nullptr;
+	};
+	Node* head = nullptr;
+	Node* tail = nullptr;
+public: 
+	Queuelst() = default;
+	Queuelst(Queuelst& src);
+	Queuelst(Queuelst&& src);
+	~Queuelst() = default;
+	[[nodiscard]] void push(Complex src) noexcept;
+	void pop();
+	[[nodiscard]] Complex& top();
+	[[nodiscard]] const Complex& top() const&;
+	bool IsEmpty() noexcept;
+	void clear() noexcept;
 
+};
+
+
+#endif
